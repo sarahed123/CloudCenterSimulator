dbe9c3667c512985f55aef5ca9a0dfc88b0db7bf
Inon Kaplan <inonkp@gmail.com>
2017-11-15 21:30:15 -0800

    starting work on central routing

---
+++ src/main/java/ch/ethz/systems/netbench/xpt/sourcerouting/StaticSourceRoutingSwitch.java
@@ -0,0 +1,91 @@
+package ch.ethz.systems.netbench.xpt.sourcerouting;
+
+import java.util.List;
+
+import ch.ethz.systems.netbench.core.Simulator;
+import ch.ethz.systems.netbench.core.network.Intermediary;
+import ch.ethz.systems.netbench.core.network.Packet;
+import ch.ethz.systems.netbench.core.network.TransportLayer;
+import ch.ethz.systems.netbench.core.run.routing.RemoteRoutingController;
+import ch.ethz.systems.netbench.ext.basic.TcpPacket;
+
+public class StaticSourceRoutingSwitch extends SourceRoutingSwitch {
+	RemoteRoutingController mRemopteRouter;
+	StaticSourceRoutingSwitch(int identifier, TransportLayer transportLayer, int n, Intermediary intermediary) {
+		super(identifier, transportLayer, n, intermediary);
+		mRemopteRouter = RemoteRoutingController.getInstance();
+	}
+	
+	/**
+     * Receives a TCP packet from the transport layer, which
+     * is oblivious to the source routing happening underneath.
+     * The TCP packet is then encapsulated to carry information of the
+     * route it must take. The sequential hash of the packet is used
+     * to determine the path it should be sent on.
+     *
+     * @param genericPacket     TCP packet instance
+     */
+    @Override
+    public void receiveFromIntermediary(Packet genericPacket) {
+        TcpPacket packet = (TcpPacket) genericPacket;
+        
+	    // Retrieve possible valiant paths to choose from
+        List<SourceRoutingPath> possibilities;
+        SourceRoutingPath selectedPath;
+
+        if (isWithinExtendedTopology) {
+
+            // Determine source and destination ToR to which the source and destination servers are attached
+            int sourceTor = Simulator.getConfiguration().getGraphDetails().getTorIdOfServer(packet.getSourceId());
+            int destinationTor = Simulator.getConfiguration().getGraphDetails().getTorIdOfServer(packet.getDestinationId());
+
+            // Create path
+            selectedPath = new SourceRoutingPath();
+
+            // If the servers are not on the same ToR
+            if (sourceTor != destinationTor) {
+
+                // Retrieve ToR to which it is attached
+                SourceRoutingSwitch sourceTorDevice = (SourceRoutingSwitch) this.targetIdToOutputPort.get(sourceTor).getTargetDevice();
+
+                selectedPath = 
+                // Retrieve the src-ToR to dst-ToR path possibilities
+                possibilities = sourceTorDevice.getPathsList().get(destinationTor);
+
+                // Select a path out of all possibilities...
+                SourceRoutingPath orgPath = possibilities.get(packet.getHash(this.identifier) % possibilities.size());
+
+                // ... and add the path to the selected path for its own local copy
+                selectedPath.addAll(orgPath);
+
+            } else {
+
+                // If both servers are in the same ToR just create the single up-down path
+                selectedPath.add(sourceTor);
+
+            }
+
+            // Now we add both the original server and the destination server to the path
+            selectedPath.add(0, packet.getSourceId());
+            selectedPath.add(packet.getDestinationId());
+
+        } else {
+
+            // If it is not extended, just use stored paths (all ToRs are servers themselves)
+            possibilities = destinationToPaths.get(packet.getDestinationId());
+            selectedPath = possibilities.get(packet.getHash(this.identifier) % possibilities.size());
+
+        }
+        
+        // Create encapsulation to propagate through the network
+        SourceRoutingEncapsulation encapsulation = new SourceRoutingEncapsulation(
+                packet,
+                selectedPath
+        );
+        
+	    // Send to network
+        receive(encapsulation);
+
+    }
+
+}
--- src/main/java/ch/ethz/systems/netbench/xpt/xpander/XpanderRouter.java
+++ src/main/java/ch/ethz/systems/netbench/xpt/xpander/XpanderRouter.java
@@ -8,26 +8,30 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
 import ch.ethz.systems.netbench.core.Simulator;
+import ch.ethz.systems.netbench.core.run.routing.RemoteRoutingController;
 import edu.asu.emit.algorithm.graph.Graph;
 import edu.asu.emit.algorithm.graph.Path;
 import edu.asu.emit.algorithm.graph.VariableGraph;
 import edu.asu.emit.algorithm.graph.Vertex;
 import edu.asu.emit.algorithm.graph.algorithms.DijkstraShortestPathAlg;
 
-public class XpanderRouter {
+public class XpanderRouter extends RemoteRoutingController{
 	private HashSet<Path> mPaths;
+	private VariableGraph mG;
 	
 	public XpanderRouter(){
+		mG = (VariableGraph) Simulator.getConfiguration().getGraph();
 		mPaths = new HashSet<Path>();
 	}
-	public Path getRoute(int source,int dest){
-		VariableGraph G = (VariableGraph) Simulator.getConfiguration().getGraph();
-		DijkstraShortestPathAlg dijkstra = new DijkstraShortestPathAlg(G);
-		Path p = dijkstra.getShortestPath(G.getVertex(source), G.getVertex(dest));
+	
+	@Override
+	public Path getRoute(int source,int dest){ 
+		DijkstraShortestPathAlg dijkstra = new DijkstraShortestPathAlg(mG);
+		Path p = dijkstra.getShortestPath(mG.getVertex(source), mG.getVertex(dest));
 		List<Vertex> pathAsList = p.getVertexList();
 		int curr = pathAsList.get(0).getId();
 		for(int i = 1; i<pathAsList.size();i++){
-			G.deleteEdge(new ImmutablePair<Integer, Integer>(curr, pathAsList.get(i).getId()));
+			mG.deleteEdge(new ImmutablePair<Integer, Integer>(curr, pathAsList.get(i).getId()));
 			curr = pathAsList.get(i).getId();
 		}
 		mPaths.add(p);
