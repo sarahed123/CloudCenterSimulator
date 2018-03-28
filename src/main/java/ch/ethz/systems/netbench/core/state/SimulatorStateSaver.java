840e142fc44c2260b8f03800222d50e260c8d0b9
Inon Kaplan <inonkp@gmail.com>
2018-03-28 11:28:51 +0300

    started on state saving

---
+++ src/main/java/ch/ethz/systems/netbench/core/state/SimulatorStateSaver.java
@@ -0,0 +1,16 @@
+package ch.ethz.systems.netbench.core.state;
+
+import java.util.PriorityQueue;
+
+import ch.ethz.systems.netbench.core.Simulator;
+import ch.ethz.systems.netbench.core.network.Event;
+
+public class SimulatorStateSaver {
+
+	public static void save(PriorityQueue<Event> eventQueue) {
+		long time = Simulator.getCurrentTime();
+		
+		
+	}
+
+}
