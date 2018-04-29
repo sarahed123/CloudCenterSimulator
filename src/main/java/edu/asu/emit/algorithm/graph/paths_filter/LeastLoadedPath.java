ba9daf370b3536aaf99c3487945f4721f87745c7
inonkp <inonkp@gmail.com>
2018-04-29 05:27:06 -0700

    corrected least and most loaded path again

--- src/main/java/edu/asu/emit/algorithm/graph/paths_filter/LeastLoadedPath.java
+++ src/main/java/edu/asu/emit/algorithm/graph/paths_filter/LeastLoadedPath.java
@@ -14,19 +14,19 @@ public class LeastLoadedPath extends PathsFilter {
 
 	@Override
 	public Path filterPaths(Paths paths) {
-		double capacity = 0;
+		double weight = 0;
 		Path ret = new Path(0);
 		for(Path p : paths.getPaths()) {
 			double tmp = 0;
-			for(int i = 0; i<p.getVertexList().size()-1;i++) {
-				Vertex curr = p.getVertexList().get(i);
-				Vertex next = p.getVertexList().get(i+1);
+			for(Vertex v : p.getVertexList()) {
+				for(Vertex u : G.getAdjacentVertices(v)) {
+					tmp+= G.getEdgeCapacity(v, u);
+				}
 				
-				tmp+= G.getEdgeCapacity(curr, next);
 				
 			}
-			if(capacity < tmp) {
-				capacity = tmp;
+			if(weight < tmp) {
+				weight = tmp;
 				ret = p;
 			}
 		}
