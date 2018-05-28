8535395f763ceda6849b486fb990f70eafa15477
Inon Kaplan <inonkp@gmail.com>
2018-05-28 18:31:08 +0300

    continued working on fat tree dijkstra

--- src/test/java/ch/ethz/systems/netbench/xpt/algorithms/FatTreePathsAlgTest.java
+++ src/test/java/ch/ethz/systems/netbench/xpt/algorithms/FatTreePathsAlgTest.java
@@ -15,6 +15,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Set;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FatTreePathsAlgTest {
@@ -22,23 +23,60 @@ public class FatTreePathsAlgTest {
     MockedFatTreeAlg alg;
     @Before
     public void setup() throws IOException {
-        Pair<Graph, GraphDetails> result = GraphReader.read("example/topologies/fat_tree/fat_tree_k14.topology");
-        g = result.getLeft();
+        
         Vertex.setTieBreaker(new VertexWeightTieBreaker() {
             @Override
             public int breakTie(Vertex vertex, Vertex vertex1) {
                 return 0;
             }
         });
-        alg = new MockedFatTreeAlg(g,4);
+        
     }
 
 
     @Test
-    public void testPredecessorMap(){
+    public void testPathFromVToLevel(){
+    	Pair<Graph, GraphDetails> result = GraphReader.read("example/topologies/fat_tree/fat_tree_k4.topology");
+        g = result.getLeft();
+        alg = new MockedFatTreeAlg(g,4);
         HashMap<Vertex,Vertex> predecessors = new HashMap<>();
-        alg.getPathFromVToLevel(new Vertex(0),2,predecessors);
-        Arrays.toString(predecessors.entrySet().toArray());
+        Set<Vertex> coreSet = alg.getPathFromVToLevel(new Vertex(5),2,predecessors);
+        assert(Arrays.toString(coreSet.toArray()).equals("[16, 17, 18, 19]"));
+        assert(Arrays.toString(predecessors.entrySet().toArray()).equals("[16=12, 17=12, 18=13, 19=13, 12=5, 13=5]"));
+        
+        predecessors.clear();
+        coreSet = alg.getPathFromVToLevel(new Vertex(5),1,predecessors);
+        //System.out.println(Arrays.toString(predecessors.entrySet().toArray()));
+        assert(Arrays.toString(coreSet.toArray()).equals("[12, 13]"));
+        assert(Arrays.toString(predecessors.entrySet().toArray()).equals("[12=5, 13=5]"));
+        
+        predecessors.clear();
+        coreSet = alg.getPathFromVToLevel(new Vertex(5),0,predecessors);
+        assert(Arrays.toString(coreSet.toArray()).equals("[5]"));
+        
+        result = GraphReader.read("example/topologies/fat_tree/fat_tree_k8.topology");
+        g = result.getLeft();
+        alg = new MockedFatTreeAlg(g,8);
+        predecessors = new HashMap<>();
+        coreSet = alg.getPathFromVToLevel(new Vertex(5),2,predecessors);
+       // System.out.println(Arrays.toString(predecessors.entrySet().toArray()));
+       // System.out.println(Arrays.toString(coreSet.toArray()));
+        assert(Arrays.toString(coreSet.toArray()).equals("[64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79]"));
+        assert(Arrays.toString(predecessors.entrySet().toArray()).equals("[64=36, 65=36, 66=36, 67=36, 36=5, 68=37, 37=5, 69=37, 38=5, 70=37, 39=5, 71=37, 72=38, 73=38, 74=38, 75=38, 76=39, 77=39, 78=39, 79=39]"));
+        
+        predecessors.clear();
+        coreSet = alg.getPathFromVToLevel(new Vertex(5),1,predecessors);
+       // System.out.println(Arrays.toString(predecessors.entrySet().toArray()));
+       // System.out.println(Arrays.toString(coreSet.toArray()));
+        assert(Arrays.toString(coreSet.toArray()).equals("[36, 37, 38, 39]"));
+        assert(Arrays.toString(predecessors.entrySet().toArray()).equals("[36=5, 37=5, 38=5, 39=5]"));
+        
+        predecessors.clear();
+        
+        coreSet = alg.getPathFromVToLevel(new Vertex(5),0,predecessors);
+        //System.out.println(Arrays.toString(coreSet.toArray()));
+        assert(Arrays.toString(coreSet.toArray()).equals("[5]"));
+
 
 
     }
