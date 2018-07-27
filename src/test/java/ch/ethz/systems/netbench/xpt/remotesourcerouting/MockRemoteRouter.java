f2ce9c48e457d668e9d030c0af946f10d468770a
Inon Kaplan <inonkp@gmail.com>
2018-07-27 14:10:34 +0300

    continued with jumboflow testing

--- src/test/java/ch/ethz/systems/netbench/xpt/remotesourcerouting/MockRemoteRouter.java
+++ src/test/java/ch/ethz/systems/netbench/xpt/remotesourcerouting/MockRemoteRouter.java
@@ -1,7 +1,9 @@
 package ch.ethz.systems.netbench.xpt.remotesourcerouting;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import ch.ethz.systems.netbench.core.Simulator;
 import ch.ethz.systems.netbench.core.config.NBProperties;
 import ch.ethz.systems.netbench.core.network.NetworkDevice;
 import ch.ethz.systems.netbench.core.run.routing.remote.RemoteRoutingController;
@@ -9,11 +11,17 @@ import ch.ethz.systems.netbench.xpt.xpander.XpanderRouter;
 import edu.asu.emit.algorithm.graph.Path;
 import edu.asu.emit.algorithm.graph.algorithms.DijkstraShortestPathAlg;
 import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 public class MockRemoteRouter extends XpanderRouter {
 
+	HashMap<Pair<Integer,Integer>,Boolean> routed;
+	HashMap<Pair<Integer,Integer>,Boolean> recovered;
+
 	public MockRemoteRouter(Map<Integer, NetworkDevice> idToNetworkDevice,NBProperties configuration) {
 		super(idToNetworkDevice, configuration);
+		routed = new HashMap<>();
+		recovered = new HashMap<>();
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -23,6 +31,13 @@ public class MockRemoteRouter extends XpanderRouter {
 		super.switchPath(src, dst, newPath, flowId);
 	}
 	
+	@Override
+	public void recoverPath(int src,int dst){
+		super.recoverPath(src, dst);
+		recovered.put(new ImmutablePair<Integer, Integer>(src, dst), true);
+	}
+	
+	@Override
 	protected Path generatePathFromGraph(int source,int dest) {
 		
 		return super.generatePathFromGraph(source, dest);
@@ -32,7 +47,28 @@ public class MockRemoteRouter extends XpanderRouter {
 		return  mPaths.get(new ImmutablePair<>(src,dst));
 	}
 	
+	@Override
+	protected void updateForwardingTables(int source, int dest, Path p, long flowId) {
+		super.updateForwardingTables(source, dest, p, flowId);
+		routed.put(new ImmutablePair<Integer, Integer>(source, dest), true);
+	}
+
+	public boolean routed(int source, int dest) {
+		// TODO Auto-generated method stub
+		return routed.getOrDefault(new ImmutablePair<Integer, Integer>(source, dest), false);
+	}
 	
+	public boolean recovered(int source, int dest) {
+		// TODO Auto-generated method stub
+		return recovered.getOrDefault(new ImmutablePair<Integer, Integer>(source, dest), false);
+	}
+	
+	@Override
+	public void reset(){
+		super.reset();
+		recovered.clear();
+		routed.clear();
+	}
 
 
 }
