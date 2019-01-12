cb820b8f2f932d7f6a91c5f3e25046167c6313ae
Inon Kaplan <inonkp@gmail.com>
2019-01-12 11:54:37 +0200

    investigating the eps

--- src/main/java/ch/ethz/systems/netbench/xpt/megaswitch/server_optic/distributed/DistributedProtocolPort.java
+++ src/main/java/ch/ethz/systems/netbench/xpt/megaswitch/server_optic/distributed/DistributedProtocolPort.java
@@ -10,21 +10,44 @@ import java.util.LinkedList;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class DistributedProtocolPort extends EcnTailDropOutputPort {
+	private int numRpPackets;
     public DistributedProtocolPort(NetworkDevice ownNetworkDevice, NetworkDevice targetNetworkDevice, Link link, long maxQueueSizeBytes, long ecnThresholdKBytes) {
-        super(ownNetworkDevice, targetNetworkDevice, link, maxQueueSizeBytes, ecnThresholdKBytes, new LinkedList());
+        super(ownNetworkDevice, targetNetworkDevice, link, maxQueueSizeBytes, ecnThresholdKBytes, new LinkedList<Packet>());
+        numRpPackets = 0;
     }
+    
+    protected void log(Packet packet) {
+    	if(getBufferOccupiedBits() >= ecnThresholdKBits * 4) {
+    		System.out.println("Port on device " + this.getOwnId() + " has surpassed ecn thresh with " + numRpPackets + " rp packets");
+    	}
+		
+	}
 
     @Override
     protected void addPacketToQueue(Packet packet){
         try{
             ReservationPacket rp = (ReservationPacket) packet;
             ((LinkedList<Packet>)queue).addFirst(packet);
+            numRpPackets++;
             return;
         }catch (ClassCastException e){
 
         }
         super.addPacketToQueue(packet);
     }
+    
+    protected Packet popFromQueue() {
+		// TODO Auto-generated method stub
+    	Packet p = super.popFromQueue();
+    	try{
+            ReservationPacket rp = (ReservationPacket) p;
+            numRpPackets--;
+    	}catch(ClassCastException e) {
+    		
+    	}
+    	
+		return p;
+	}
     
     @Override
     protected boolean hasBufferSpace(IpHeader packet) {
