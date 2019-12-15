b316c5dd8ea87f6160c443a3c2417ce01e8a27c7
Inon Kaplan <inonkp@gmail.com>
2019-12-15 13:14:06 +0200

    updated matchings script for first in two step symmetric matching

--- xpander_generation/opera/create_matchings.py
+++ xpander_generation/opera/create_matchings.py
@@ -34,46 +34,101 @@ def switch_prev(matching, curr_row_index):
 
 output_file = sys.argv[1]
 matchings_num = int(sys.argv[2])
+sys.setrecursionlimit(15000)
+def find_matchings_row_recursive(row_num):
+    if row_num >= len(matrix):
+        return True
+    ret = False
+    print(row_num)
+    while not ret:
+
+        elem_from_choices = [i for i in range(len(matrix[row_num])) if matrix[row_num][i]==-1]
+
+        while elem_from_choices:
+
+            elem_from = random.choice(elem_from_choices)
+
+            elem_to_choices = [i for i in range(len(matrix[row_num])) if matrix[row_num][i]==-1]
+            while elem_to_choices:
+
+                elem_to = random.choice(elem_to_choices)
+                colunm = [matrix[i][elem_to] for i in range(len(matrix))]
+                if elem_from in colunm:
+                    if row_num==0:
+                        print(matrix)
+                        print(elem_to_choices)
+                    elem_to_choices.remove(elem_to)
+                    continue
+
+                matrix[row_num][elem_to] = elem_from
+                matrix[row_num][elem_from] = elem_to
+                elem_from_choices.remove(elem_from)
+                if elem_from!=elem_to:
+                    elem_from_choices.remove(elem_to)
+                break
+            if matrix[row_num][elem_from]==-1:
+                matrix[row_num] = [-1 for i in range(matchings_num)]
+                return False
 
 
-matrix = [[i for i in range(matchings_num)] for j in range(matchings_num) ]
+        ret = find_matchings(row_num+1)
+        if not ret:
+            matrix[row_num] = [-1 for i in range(matchings_num)]
+        else:
+            return True
+    
+
+def find_matchings_element_recursive(row_num):
+    if row_num >= len(matrix):
+        return True
+    elem_from_choices = [i for i in range(len(matrix[row_num])) if matrix[row_num][i]==-1]
+    while elem_from_choices:
+        elem_from = random.choice(elem_from_choices)
+        elem_to_choices = [i for i in range(len(matrix[row_num])) if matrix[row_num][i]==-1]
+        while elem_to_choices:
+            elem_to = random.choice(elem_to_choices)
+            colunm = [matrix[i][elem_to] for i in range(len(matrix))]
+
+            if elem_from in colunm:
+                elem_to_choices.remove(elem_to)
+                continue
+            matrix[row_num][elem_to] = elem_from
+            matrix[row_num][elem_from] = elem_to
+            elem_to_choices.remove(elem_to)
+            new_row_num = row_num if -1 in matrix[row_num] else row_num+1
+            ret = find_matchings_element_recursive(new_row_num)
+            if not ret:
+                matrix[row_num][elem_to] = -1
+                matrix[row_num][elem_from] = -1
+                continue
+            return True
+        return False
+    return False    
+    
+
+
+
+matrix = [[-1 for i in range(matchings_num)] for j in range(matchings_num) ]
 
 matchings = []
-for i in range(len(matrix)):
-    matching = [-1 for k in range(matchings_num)]
-    while -1 in matching:
-        j = matching.index(-1)
-        row = matrix[j]
-        if matching[j] != -1:
-            continue
-        choices = list(filter(lambda x: x not in matching,row))
-        if not choices:
-            matching[j] = j
-            continue
-            
-        elem = random.choice(choices)
-        row.remove(elem)
-        matching[j] = elem
-        if elem!=j:
-            matching[elem] = j
-            matrix[elem].remove(j)
-        
-    print(f"matching {i} complete")
-    matchings.append(matching)
+print(find_matchings_element_recursive(0))
 
+print(matrix)
 edges = {}
 for i in range(len(matrix)):
     edges[i] = []
-for j,m in enumerate(matchings):
+for j,m in enumerate(matrix):
     assert len(set(m))==len(matrix)
     #check disjoint
     for i in range(len(m)):
+
+        assert i == m[m[i]], str(m) + " " + str(i)
         if(i!=m[i]):
             assert not m[i] in edges[i]
         edges[i].append(m[i])
 
 with open(output_file, "w") as f:
-    for m in matchings:
+    for m in matrix:
         for i in m:
             f.write(str(i) + " ")
         f.write("\n")
