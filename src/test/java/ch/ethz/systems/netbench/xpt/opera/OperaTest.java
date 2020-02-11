ae8f48867911b054d3663258ed5f4e253abaa5e5
inonkp <inonkp@gmail.com>
2020-02-11 15:48:09 +0200

    updated opera test

--- src/test/java/ch/ethz/systems/netbench/xpt/opera/OperaTest.java
+++ src/test/java/ch/ethz/systems/netbench/xpt/opera/OperaTest.java
@@ -3,10 +3,13 @@ package ch.ethz.systems.netbench.xpt.opera;
 import ch.ethz.systems.netbench.core.Simulator;
 import ch.ethz.systems.netbench.core.config.BaseAllowedProperties;
 import ch.ethz.systems.netbench.core.config.NBProperties;
-import ch.ethz.systems.netbench.core.network.NetworkDevice;
-import ch.ethz.systems.netbench.core.network.TransportLayer;
+import ch.ethz.systems.netbench.core.config.TopologyServerExtender;
+import ch.ethz.systems.netbench.core.config.exceptions.PropertyValueInvalidException;
+import ch.ethz.systems.netbench.core.log.SimulationLogger;
+import ch.ethz.systems.netbench.core.network.*;
 import ch.ethz.systems.netbench.core.run.infrastructure.BaseInitializer;
 import ch.ethz.systems.netbench.core.run.traffic.TrafficPlanner;
+import ch.ethz.systems.netbench.ext.basic.EcnTailDropOutputPort;
 import ch.ethz.systems.netbench.ext.demo.DemoIntermediary;
 import ch.ethz.systems.netbench.ext.flowlet.UniformFlowletIntermediary;
 import ch.ethz.systems.netbench.ext.poissontraffic.PoissonArrivalPlanner;
@@ -14,6 +17,7 @@ import ch.ethz.systems.netbench.ext.poissontraffic.flowsize.PFabricWebSearchUppe
 import ch.ethz.systems.netbench.xpt.dynamic.opera.OperaController;
 import ch.ethz.systems.netbench.xpt.dynamic.opera.OperaSwitch;
 import ch.ethz.systems.netbench.xpt.simple.simpledctcp.SimpleDctcpTransportLayer;
+import ch.ethz.systems.netbench.xpt.simple.simpleserver.SimpleServer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,38 +29,125 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Random;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OperaTest {
     MockOperaController operaController;
     NBProperties configuration;
-    long simulationDuration = 100000;
+    long simulationDuration = 1000000000;
     @Before
     public void setup() throws IOException {
         configuration = new NBProperties(BaseAllowedProperties.EXPERIMENTAL,BaseAllowedProperties.PROPERTIES_RUN,
                                                         BaseAllowedProperties.EXTENSION, BaseAllowedProperties.LOG);
         configuration.setProperty("opera_rotors_dir_path","xpander_generation/opera/testing/n20/rotors");
-        configuration.setProperty("opera_direct_circuit_threshold_byte", "100000");
-        configuration.setProperty("opera_reconfiguration_time_ns","1000");
+        configuration.setProperty("opera_direct_circuit_threshold_byte", "40000");
+        configuration.setProperty("opera_reconfiguration_time_ns","20000000");
         configuration.setProperty("opera_parallel_rotors_to_config","1");
-        configuration.setProperty("opera_routing_tables_dir_path","xpander_generation/opera/testing/n20/routing");
+        configuration.setProperty("opera_routing_tables_dir_path","xpander_generation/opera/testing/n20/k_shortest_routing");
         configuration.setProperty("scenario_topology_file","xpander_generation/opera/testing/n20/n20.topology");
-        configuration.setProperty("link_delay_ns", "250");
-        configuration.setProperty("FLOWLET_GAP_NS", "5000000");
+        configuration.setProperty("link_delay_ns", "0");
+        configuration.setProperty("FLOWLET_GAP_NS", "50000");
         configuration.setProperty("run_folder_name", "opera");
         configuration.setProperty("run_folder_base_dir", "temp");
         configuration.setProperty("link_bandwidth_bit_per_ns", "100");
+        configuration.setProperty("scenario_topology_extend_with_servers", "regular");
+        configuration.setProperty("scenario_topology_extend_servers_per_tl_node","1");
         Simulator.setup(1,configuration);
+
+        if (configuration.isPropertyDefined("scenario_topology_extend_with_servers")) {
+            if (configuration.getPropertyWithDefault("scenario_topology_extend_with_servers", "").equals("regular")) {
+
+                // Number of servers to add to each transport layer node
+                int serversPerNodeToExtendWith = configuration.getIntegerPropertyOrFail("scenario_topology_extend_servers_per_tl_node");
+
+                // Extend topology
+                new TopologyServerExtender(
+                        configuration.getTopologyFileNameOrFail(),
+                        "temp/opera/" + "/extended_topology.txt"
+                ).extendRegular(serversPerNodeToExtendWith);
+
+
+                configuration.markExtended();
+            } else {
+                throw new PropertyValueInvalidException(configuration, "scenario_topology_extend_with_servers");
+            }
+
+            // Override configuration property
+            configuration.overrideProperty("scenario_topology_file", "temp/opera/" + "/extended_topology.txt");
+
+        }
 
         HashMap<Integer, TransportLayer> transportLayerHashMap = new HashMap<>();
         HashMap<Integer, NetworkDevice> networkDeviceHashMap = new HashMap<>();
-        for(int i =0; i<20; i++){
+        for(int i =0; i<40; i++){
             SimpleDctcpTransportLayer transportLayer = new SimpleDctcpTransportLayer(i,configuration);
-            OperaSwitch os = new OperaSwitch(i,transportLayer,new UniformFlowletIntermediary(configuration),configuration);
-            transportLayer.setNetworkDevice(os);
+            NetworkDevice os;
+            if(i<20){
+                os = new OperaSwitch(i,null,null,configuration);
+            }else{
+                os = new SimpleServer(i,transportLayer,new UniformFlowletIntermediary(configuration),configuration);
+                InputPort portBtoA = new InputPort(os, networkDeviceHashMap.get(i - 20), new Link() {
+                    @Override
+                    public long getDelayNs() {
+                        return 0;
+                    }
+
+                    @Override
+                    public long getBandwidthBitPerNs() {
+                        return 100;
+                    }
+
+                    @Override
+                    public boolean doesNextTransmissionFail(long packetSizeBits) {
+                        return false;
+                    }
+                });
+                os.addIncomingConnection(portBtoA);
+                networkDeviceHashMap.get(i-20).addConnection(new EcnTailDropOutputPort(networkDeviceHashMap.get(i-20), os , new Link() {
+                    @Override
+                    public long getDelayNs() {
+                        return 0;
+                    }
+
+                    @Override
+                    public long getBandwidthBitPerNs() {
+                        return 100;
+                    }
+
+                    @Override
+                    public boolean doesNextTransmissionFail(long packetSizeBits) {
+                        return false;
+                    }
+                },150000000,3000000,new LinkedList<>()) {
+
+                });
+                os.addConnection(new EcnTailDropOutputPort(os, networkDeviceHashMap.get(i-20), new Link() {
+                    @Override
+                    public long getDelayNs() {
+                        return 0;
+                    }
+
+                    @Override
+                    public long getBandwidthBitPerNs() {
+                        return 100;
+                    }
+
+                    @Override
+                    public boolean doesNextTransmissionFail(long packetSizeBits) {
+                        return false;
+                    }
+                },150000000,3000000,new LinkedList<>()) {
+
+                });
+                transportLayer.setNetworkDevice(os);
+                transportLayerHashMap.put(i,transportLayer);
+            }
+
+
             networkDeviceHashMap.put(i,os);
-            transportLayerHashMap.put(i,transportLayer);
+
         }
 
         // operaController = new MockOperaController(configuration,networkDeviceHashMap);
@@ -66,16 +157,15 @@ public class OperaTest {
         TrafficPlanner planner = new TrafficPlanner(transportLayerHashMap, configuration) {
             @Override
             public void createPlan(long durationNs) {
-                Random random = new Random();
-                registerFlow(0, 0, 19, 10000000);
+                Random random = Simulator.selectIndependentRandom("opera_test_traffic_planner");
 
-                for(long t = 0; t<durationNs; t+=40){
-                    int source = Math.abs(random.nextInt()) % 20;
-                    int dest = Math.abs(random.nextInt()) % 20;
+                for(long t = 0; t<durationNs; t+=durationNs){
+                    int source = Math.abs(random.nextInt(20)) % 20 + 20;
+                    int dest = Math.abs(random.nextInt(20)) % 20 + 20;
                     while(dest==source){
-                        dest = Math.abs(random.nextInt()) % 20;
+                        dest = Math.abs(random.nextInt(20)) % 20 + 20;
                     }
-                    registerFlow(t,source,dest,10000);
+                    registerFlow(t,source,dest,1000000000);
                 }
             }
         };
@@ -91,8 +181,8 @@ public class OperaTest {
 
     @Test
     public void testSomeRoutes(){
-        assert(operaController.nextHops(6,0).toString().equals("[(0,8), (1,14), (2,7), (3,12)]"));
-        assert(operaController.nextHops(7,5).toString().equals("[(3,19)]"));
+        assert(OperaController.getInstance().getPossiblities(0,6 ).toString().equals("[[(2,6)], [(0,2), (1,15), (0,12), (3,6)]]"));
+        assert(OperaController.getInstance().getPossiblities(3,4 ).toString().equals("[[(2,19), (1,4)], [(0,6), (2,0), (1,17), (0,4)]]"));
     }
 
     @Test
