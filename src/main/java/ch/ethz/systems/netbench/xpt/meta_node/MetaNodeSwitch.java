622bec5350f3af12e7036dca268c3d38c1f5e6ec
inonkp <inonkp@gmail.com>
2020-06-18 12:04:11 +0300

    Working on MN

--- src/main/java/ch/ethz/systems/netbench/xpt/meta_node/MetaNodeSwitch.java
+++ src/main/java/ch/ethz/systems/netbench/xpt/meta_node/MetaNodeSwitch.java
@@ -1,8 +1,8 @@
 package ch.ethz.systems.netbench.xpt.meta_node;
 
-import ch.ethz.systems.netbench.core.Simulator;
 import ch.ethz.systems.netbench.core.config.NBProperties;
 import ch.ethz.systems.netbench.core.network.Intermediary;
+import ch.ethz.systems.netbench.core.network.NetworkDevice;
 import ch.ethz.systems.netbench.core.network.Packet;
 import ch.ethz.systems.netbench.core.network.TransportLayer;
 import ch.ethz.systems.netbench.ext.basic.TcpHeader;
@@ -10,17 +10,15 @@ import ch.ethz.systems.netbench.ext.ecmp.EcmpSwitch;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 public class MetaNodeSwitch extends EcmpSwitch {
-    Map<Pair<Integer,Integer>, MetaNodeToken> tokenMap;
+    Map<Integer, MetaNodeToken> tokenMap;
     private Random rand;
     private int MNId;
+    private int currToRDest;
     /**
-     * Constructor for ECMP switch.
+     * Constructor for MetaNodeSwitch
      *
      * @param identifier     Network device identifier
      * @param transportLayer Underlying server transport layer instance (set null, if none)
@@ -32,36 +30,75 @@ public class MetaNodeSwitch extends EcmpSwitch {
         super(identifier, transportLayer, n, intermediary, configuration);
         MNId = -1;
         tokenMap = new HashMap<>();
-        this.rand = Simulator.selectIndependentRandom("mn_switch_randomizer");
+        currToRDest = 0;
+        if(!isServer()) currToRDest = -1;
+
     }
 
     protected void  forwardToNextSwitch(Packet genericPacket){
         // Convert to TCP packet
         TcpHeader tcpHeader = (TcpHeader) genericPacket;
         boolean isSecondHop = !targetIdToOutputPort.containsKey(tcpHeader.getSourceId());
-        if(this.isServer() || isSecondHop){
-            List<Integer> possibilities = destinationToNextSwitch.get(tcpHeader.getDestinationId());
-            int randomNext = possibilities.get(rand.nextInt(possibilities.size()));
-            this.targetIdToOutputPort.get(randomNext).enqueue(genericPacket);
+
+        if(this.isServer()){
+            forwardFromserver(genericPacket);
+            return;
+        }
+        if(isSecondHop){
+            forwardFromSecondHop(genericPacket);
             return;
         }
 
         MetaNodeToken token = getToken(tcpHeader.getDestinationId());
+        List<Integer> possibilities = getDestinationToMN(token.getMNDest());
+        int randomNext = possibilities.get(rand.nextInt(possibilities.size()));
+        this.targetIdToOutputPort.get(randomNext).enqueue(genericPacket);
+        token.nextBytes(tcpHeader.getSizeBit()/(8));
 
+    }
 
+    protected void forwardFromserver(Packet genericPacket){
+        TcpHeader tcpHeader = (TcpHeader) genericPacket;
+        assert currToRDest >= 0;
+        List<Integer> possibilities = destinationToNextSwitch.get(tcpHeader.getDestinationId());
+        int next = possibilities.get(currToRDest);
+        this.targetIdToOutputPort.get(next).enqueue(genericPacket);
+        currToRDest++;
+        currToRDest%=possibilities.size();
+        return;
+    }
 
+    protected void forwardFromSecondHop(Packet genericPacket){
+        TcpHeader tcpHeader = (TcpHeader) genericPacket;
+        List<Integer> possibilities = destinationToNextSwitch.get(tcpHeader.getDestinationId());
+        int randomNext = possibilities.get(rand.nextInt(possibilities.size()));
+        this.targetIdToOutputPort.get(randomNext).enqueue(genericPacket);
+        return;
+
+    }
+    protected List<Integer> getDestinationToMN(int mnDest) {
+        List<Integer> possiblilities = new LinkedList<>();
+        for(int i : this.targetIdToOutputPort.keySet()){
+            NetworkDevice nd = targetIdToOutputPort.get(i).getTargetDevice();
+            if(nd.isServer()) continue;
+            MetaNodeSwitch mnsw = (MetaNodeSwitch) nd;
+            if(mnsw.MNId == mnDest) possiblilities.add(mnsw.identifier);
+
+        }
+        if(possiblilities.size() == 0) throw new IllegalStateException("there are no routes to the MNDest " + mnDest);
+        return possiblilities;
     }
 
     private MetaNodeToken getToken(int destinationId) {
         MNController controller = MNController.getInstance();
         int MNSource = this.MNId;
         int MNDest = controller.getMetaNodeId(destinationToNextSwitch.get(destinationId).get(0));
-        MetaNodeToken currToken = tokenMap.get(new ImmutablePair<>(MNSource,MNDest));
+        MetaNodeToken currToken = tokenMap.get(MNDest);
         if(currToken==null){
             currToken = MNController.getInstance().getToken(MNSource, MNDest);
         }
         if(currToken.expired()){
-            currToken = MNController.getInstance().getToken(MNSource, MNDest, Math.min(currToken.KBytes*2, 1000));
+            currToken = MNController.getInstance().getToken(MNSource, MNDest, currToken.bytes);
         }
 
         return currToken;
@@ -70,5 +107,9 @@ public class MetaNodeSwitch extends EcmpSwitch {
     public void setMetaNodeId(int mn) {
         if(MNId!= -1) throw new IllegalStateException("Meta Node Id cannot be set twice");
         MNId = mn;
+    }
+
+    public void setRandomizer(Random rand) {
+        this.rand = rand;
     }
 }
--- src/main/java/ch/ethz/systems/netbench/xpt/meta_node/MetaNodeToken.java
+++ src/main/java/ch/ethz/systems/netbench/xpt/meta_node/MetaNodeToken.java
@@ -4,16 +4,14 @@ import ch.ethz.systems.netbench.core.Simulator;
 
 public class MetaNodeToken {
     int dest;
-    long KBytes;
-    private long currKbytes;
+    long bytes;
     private long timeoutNs;
     private boolean expired;
     private long lastUsed;
     private MetaNodeTokeTimeoutEvent timeoutEvent;
-    public MetaNodeToken(long KBytes, int dest, long timeoutNs) {
+    public MetaNodeToken(long bytes, int dest, long timeoutNs) {
         this.dest = dest;
-        this.KBytes = KBytes;
-        this.currKbytes = 0;
+        this.bytes = bytes;
         this.timeoutNs = timeoutNs;
         timeoutEvent = new MetaNodeTokeTimeoutEvent(timeoutNs,this);
         Simulator.registerEvent(timeoutEvent);
@@ -22,15 +20,15 @@ public class MetaNodeToken {
     }
 
     public boolean expired() {
-        return KBytes<=0 || this.expired;
+        return bytes <=0 || this.expired;
     }
 
     public void setExpired(){
         this.expired = true;
     }
 
-    public void update(long nextKBytes){
-        KBytes-=nextKBytes;
+    public void nextBytes(long nextBytes){
+        bytes -=nextBytes;
         lastUsed = Simulator.getCurrentTime();
 
     }
@@ -46,4 +44,9 @@ public class MetaNodeToken {
     public boolean active() {
         return (Simulator.getCurrentTime() - lastUsed()) <= getTimeout();
     }
+
+    public int getMNDest(){
+        return dest;
+    }
+
 }
