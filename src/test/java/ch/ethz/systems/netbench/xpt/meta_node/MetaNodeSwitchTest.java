20de63183c1c36b6e661396c000862ebad376cdf
inonkp <inonkp@gmail.com>
2020-06-18 12:03:37 +0300

    working on MN tests

--- src/test/java/ch/ethz/systems/netbench/xpt/meta_node/MetaNodeSwitchTest.java
+++ src/test/java/ch/ethz/systems/netbench/xpt/meta_node/MetaNodeSwitchTest.java
@@ -1,4 +1,179 @@
 package ch.ethz.systems.netbench.xpt.meta_node;
 
+import ch.ethz.systems.netbench.core.Simulator;
+import ch.ethz.systems.netbench.core.config.BaseAllowedProperties;
+import ch.ethz.systems.netbench.core.config.NBProperties;
+import ch.ethz.systems.netbench.core.network.*;
+import ch.ethz.systems.netbench.core.run.infrastructure.*;
+import ch.ethz.systems.netbench.ext.bare.BarePacket;
+import ch.ethz.systems.netbench.ext.basic.TcpPacket;
+import ch.ethz.systems.netbench.xpt.megaswitch.Encapsulatable;
+import ch.ethz.systems.netbench.xpt.simple.simpleserver.SimpleServer;
+import ch.ethz.systems.netbench.xpt.tcpbase.FullExtTcpPacket;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MetaNodeSwitchTest {
+    private File tempRunConfig;
+    MockMNController instance;
+    @Mock
+    MockTcpPacket tcpPacket;
+
+    @Before
+    public void setup() throws IOException {
+        tempRunConfig = File.createTempFile("temp-run-config", ".tmp");
+        BufferedWriter runConfigWriter = new BufferedWriter(new FileWriter(tempRunConfig));
+        runConfigWriter.write("scenario_topology_file=./src/test/java/ch/ethz/systems/netbench/xpt/meta_node/S50_O0.5_L5_D6.topology\n");
+        runConfigWriter.write("link_bandwidth_bit_per_ns=50\n");
+        runConfigWriter.write("link_delay_ns=10\n");
+        runConfigWriter.close();
+        NBProperties conf = new NBProperties(
+                tempRunConfig.getAbsolutePath(),
+                BaseAllowedProperties.LOG,
+                BaseAllowedProperties.PROPERTIES_RUN,
+                BaseAllowedProperties.EXTENSION,
+                BaseAllowedProperties.EXPERIMENTAL,
+                BaseAllowedProperties.BASE_DIR_VARIANTS
+        );
+        Simulator.setup(0,conf);
+        BaseInitializer initializer = BaseInitializer.getInstance();
+        // 1.1) Generate nodes
+        initializer.extend(
+                0,
+                new OutputPortGenerator(conf) {
+                    @Override
+                    public OutputPort generate(NetworkDevice networkDevice, NetworkDevice networkDevice1, Link link) {
+                        return new OutputPort(networkDevice,networkDevice1 , link, new LinkedList<>()) {
+                            @Override
+                            public void enqueue(Packet packet) {
+                                addPacketToQueue(packet);
+                            }
+                        };
+                    }
+                },
+                new NetworkDeviceGenerator(conf) {
+                    @Override
+                    public NetworkDevice generate(int i) {
+                        return new MockMetaNodeSwitch(i, null, conf.getGraphDetails().getNumNodes(), new Intermediary() {
+                            @Override
+                            public Packet adaptOutgoing(Packet packet) {
+                                return null;
+                            }
+
+                            @Override
+                            public Packet adaptIncoming(Packet packet) {
+                                return null;
+                            }
+                        }, conf) {
+
+
+                            @Override
+                            public void receiveFromIntermediary(Packet packet) {
+
+                            }
+                        };
+                    }
+
+                    @Override
+                    public NetworkDevice generate(int i, TransportLayer transportLayer) {
+                        return new MockMetaNodeSwitch(i, transportLayer, conf.getGraphDetails().getNumNodes(), new Intermediary() {
+                            @Override
+                            public Packet adaptOutgoing(Packet packet) {
+                                return null;
+                            }
+
+                            @Override
+                            public Packet adaptIncoming(Packet packet) {
+                                return null;
+                            }
+                        },conf);
+                    }
+                },
+                new LinkGenerator() {
+                    @Override
+                    public Link generate(NetworkDevice networkDevice, NetworkDevice networkDevice1) {
+                        return null;
+                    }
+                },
+                new TransportLayerGenerator(conf) {
+                    @Override
+                    public TransportLayer generate(int i) {
+                        return new TransportLayer(i,conf) {
+                            @Override
+                            protected Socket createSocket(long l, int i, long l1) {
+                                return null;
+                            }
+                        };
+                    }
+                }
+        );
+        initializer.createInfrastructure(conf);
+        instance = MockMNController.getInstance(conf,initializer.getIdToNetworkDevice());
+        instance.populateRoutingTables();
+
+    }
+
+    @Test
+    public void getDestinationToMNTest(){
+        MockMetaNodeSwitch mnsw = (MockMetaNodeSwitch) instance.getDevice(0);
+        List<Integer> dests =  Arrays.asList(5);
+
+        assert mnsw.getDestinationToMNMock(1).equals(dests);
+        dests =  Arrays.asList(10);
+        assert mnsw.getDestinationToMNMock(2).equals(dests);
+    }
+
+    @Test
+    public void forwardFromServerTest(){
+        MockMetaNodeSwitch mnsw = (MockMetaNodeSwitch) instance.getDevice(25);
+
+        Mockito.doReturn(50).when(tcpPacket).getDestinationId();
+        Mockito.doReturn(1000l).when(tcpPacket).getSizeBit();
+
+        mnsw.receive(tcpPacket);
+        assert mnsw.getTargetOuputPort(0).getQueueSize() == 1;
+        mnsw.receive(tcpPacket);
+        assert mnsw.getTargetOuputPort(1).getQueueSize() == 1;
+        mnsw.receive(tcpPacket);
+        mnsw.receive(tcpPacket);
+        mnsw.receive(tcpPacket);
+        mnsw.receive(tcpPacket);
+        assert mnsw.getTargetOuputPort(0).getQueueSize() == 2;
+
+    }
+
+    @Test
+    public void forwardFromSecondHopTest(){
+        MockMetaNodeSwitch mnsw = (MockMetaNodeSwitch) instance.getDevice(0);
+
+        Mockito.doReturn(35).when(tcpPacket).getDestinationId();
+        Mockito.doReturn(74).when(tcpPacket).getSourceId();
+
+        Mockito.doReturn(1000l).when(tcpPacket).getSizeBit();
+        mnsw.receive(tcpPacket);
+
+        assert mnsw.getTargetOuputPort(5).getQueueSize() == 1;
+
+
+    }
+
+    @After
+    public void clear(){
+        Simulator.reset(true);
+    }
 }
--- src/test/java/ch/ethz/systems/netbench/xpt/meta_node/MockMetaNodeSwitch.java
+++ src/test/java/ch/ethz/systems/netbench/xpt/meta_node/MockMetaNodeSwitch.java
@@ -1,4 +1,32 @@
 package ch.ethz.systems.netbench.xpt.meta_node;
 
-public class MockMetaNodeSwitch {
+import ch.ethz.systems.netbench.core.config.NBProperties;
+import ch.ethz.systems.netbench.core.network.Intermediary;
+import ch.ethz.systems.netbench.core.network.Packet;
+import ch.ethz.systems.netbench.core.network.TransportLayer;
+import ch.ethz.systems.netbench.ext.basic.TcpHeader;
+
+import java.util.List;
+
+public class MockMetaNodeSwitch extends MetaNodeSwitch {
+    /**
+     * Constructor for MetaNodeSwitch
+     *
+     * @param identifier     Network device identifier
+     * @param transportLayer Underlying server transport layer instance (set null, if none)
+     * @param n              Number of network devices in the entire network (for routing table size)
+     * @param intermediary   Flowlet intermediary instance (takes care of hash adaptation for flowlet support)
+     * @param configuration
+     */
+    public MockMetaNodeSwitch(int identifier, TransportLayer transportLayer, int n, Intermediary intermediary, NBProperties configuration) {
+        super(identifier, transportLayer, n, intermediary, configuration);
+    }
+
+
+    public List<Integer> getDestinationToMNMock(int MNId){
+        return super.getDestinationToMN(MNId);
+    }
+
+
+
 }
