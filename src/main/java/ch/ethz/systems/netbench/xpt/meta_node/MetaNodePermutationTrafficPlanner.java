aaece7371dde9df81f97f8ae6b0e30846aae374c
Inon Kaplan <inonkp@gmail.com>
2020-07-06 10:47:04 +0300

    why did I do shuffle and then forgot about it

--- src/main/java/ch/ethz/systems/netbench/xpt/meta_node/MetaNodePermutationTrafficPlanner.java
+++ src/main/java/ch/ethz/systems/netbench/xpt/meta_node/MetaNodePermutationTrafficPlanner.java
@@ -36,10 +36,8 @@ public class MetaNodePermutationTrafficPlanner extends PoissonArrivalPlanner {
         int p = 0;
         double serverPairNum = 0.0;
         while(p<pairNum){
-            List<Integer> serversA = getServesPerMNList(2*p);
-            
-
-            List<Integer> serversB = getServesPerMNList(2*p+1);
+            List<Integer> serversA = getServesPerMNList(MNs.get(2*p));
+            List<Integer> serversB = getServesPerMNList(MNs.get(2*p+1));
             if(configuration.getBooleanPropertyWithDefault("meta_node_same_rack_traffic",false)){
                 for(int serverA: serversA){
                     for(int serverB: serversA){
