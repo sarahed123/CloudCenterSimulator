24f3a011be6759c2ea1b10d10e2f4cc5421d0b88
Inon Kaplan <inonkp@gmail.com>
2020-07-13 07:56:27 +0300

    logging ecns

--- src/main/java/ch/ethz/systems/netbench/ext/basic/EcnTailDropOutputPort.java
+++ src/main/java/ch/ethz/systems/netbench/ext/basic/EcnTailDropOutputPort.java
@@ -49,6 +49,7 @@ public class EcnTailDropOutputPort extends OutputPort {
         // Mark congestion flag if size of the queue is too big
         if (getBufferOccupiedBits() >= ecnThresholdKBits) {
             SimulationLogger.increaseStatisticCounter("MARK_CONGESTION_ECN_THRESHOLD");
+            logger.logECNMark();
             ipHeader.markCongestionEncountered();
         }
 
