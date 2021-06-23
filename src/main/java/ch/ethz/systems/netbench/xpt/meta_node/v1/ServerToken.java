9962625783999fbb0b8e52af46d827be24aea68e
inonkp <inonkp@gmail.com>
2021-06-23 11:35:07 +0300

    added meta node transport layer

---
+++ src/main/java/ch/ethz/systems/netbench/xpt/meta_node/v1/ServerToken.java
@@ -0,0 +1,30 @@
+package ch.ethz.systems.netbench.xpt.meta_node.v1;
+
+public class ServerToken {
+    public final long flowId;
+    public final long bytes;
+    public final int sourceId;
+    public final int destinationId;
+    public boolean expired;
+    public final long expiryTime;
+    public ServerToken(long flowId, long bytes, int sourceId, int destinationId, long expiryTime) {
+        this.flowId = flowId;
+        this.bytes = bytes;
+        this.sourceId = sourceId;
+        this.destinationId = destinationId;
+        expired = false;
+        this.expiryTime = expiryTime;
+    }
+
+    public void invalidate(){
+        expired = true;
+    }
+
+    public boolean isExpired(){
+        return expired;
+    }
+
+    public static ServerToken dummyToken(){
+        return new ServerToken(-1,-1,-1,-1, -1);
+    }
+}
