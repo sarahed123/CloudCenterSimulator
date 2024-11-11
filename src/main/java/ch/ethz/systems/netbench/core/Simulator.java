3626802b2ef3763ec9c28ae0f0d595dfb347ac7b
sarahed123 <edery.sarah5@gmail.com>
2024-11-11 15:49:35 +0200

    add prints

--- src/main/java/ch/ethz/systems/netbench/core/Simulator.java
+++ src/main/java/ch/ethz/systems/netbench/core/Simulator.java
@@ -36,7 +36,7 @@ public class Simulator {
 	// Main ordered event queue (run variable)
 	private static PriorityQueue<Event> eventQueue = new PriorityQueue<>();
 	// Event queues map according to servers(source_id)
-	final static int NUM_THREADS = 2;
+	final static int NUM_THREADS = 4;
 			
 
 	private static PriorityQueue<Event>[] queuesServer = new PriorityQueue[NUM_THREADS];
@@ -278,7 +278,7 @@ public class Simulator {
 		}
 
 		// Log end
-		System.out.println("Simulation finished (simulated " + (runtimeNanoseconds / 1e9) + "s in a real-world time of "
+		System.out.println("Num thraeds: " + NUM_THREADS +"\nSimulation finished (simulated " + (runtimeNanoseconds / 1e9) + "s in a real-world time of "
 				+ ((System.currentTimeMillis() - startTime) / 1000.0) + "s).");
 
 	}
